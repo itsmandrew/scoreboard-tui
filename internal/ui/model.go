855aa62e9e147e71f0569e14fbea54f9c09cbd0f
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2026-01-30 13:42:41 -0800

    Changed spinner color lol

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -72,7 +72,7 @@ type Model struct {
 func InitialModel(apiKey string) Model {
 	s := spinner.New()
 	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(MainColor)
+	s.Style = lipgloss.NewStyle().Foreground(HighlightColor)
 
 	return Model{
 		state:   menuView,
